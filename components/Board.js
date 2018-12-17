a7e9ddc5c05cf3fbc54db48b637a2fda83ff113a
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-12-17 15:55:29 +0100

    adds setup of game

---
+++ components/Board.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Board extends React.Component {
+  state = {
+    boardArray: // generates the grid (and holds answers?)
+
+  }
+
+  // function that sets up the board?
+  // takes in number from square and checks if match?  
+
+  // end the game (win)
+
+  render() {
+    return (
+      // board render
+      // use the board state (if holds answers) and insert the values given to each square
+    )
+  }
+}
+
+export default Board;
---
+++ components/Square.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Square extends React.Component {
+  state = {
+    //state for this?
+    // not display correct number
+  };
+}
