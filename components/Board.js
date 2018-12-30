170c1c606f6187cf4d5e0715125a8e9eae32127a
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-12-30 12:41:00 +0100

    starting validation function

--- components/Board.js
+++ components/Board.js
@@ -14,18 +14,47 @@ class Board extends React.Component {
     // itereate over small grid 6 times
     for (let i = 0; i < x + 1; i++) {
       for (let j = 0; j < y + 1; j++) {
+        // console.log("this is the board:", board);
+        // this.addRandNum(board);
         board.push({ value: 0, displayed: false, id });
         id++;
       }
     }
     // do all the jazz
+    // board = this.addRandNum(board);
     board = this.addRandNum(board);
     console.log("this is the filled board:", board);
-    return board;
+    board = this.validateRow(board, x, y);
+    // console.log("this is the validated board:", board);
+    // return board;
   }
 
   addRandNum(board) {
-    return board.map(sq => (sq.value = randNum(6)));
+    console.log("you've reached addRandNum:");
+    // console.log("board in addRandNum:", board);
+    // console.log("filledBoard in addRandNum:", filledBoard);
+    const filledBoard = board.map(sq => ({
+      value: randNum(6),
+      displayed: sq.displayed,
+      id: sq.id
+    }));
+    console.log("filledBoard in addRandNum:", filledBoard);
+    return filledBoard;
+  }
+
+  validateRow(board, x, y) {
+    console.log("this is validateRow:", board);
+
+    console.log("this is the board coordinates:", board[x][y]);
+    // const validRowBoard = board.map(sq => {
+    //
+    // });
+    console.log("this is row number:", x);
+    console.log("this is square number:", y);
+    board.map(row => {
+      console.log("this is square row:", row);
+    });
+    console.log("this is validateRow");
   }
 
   render() {
